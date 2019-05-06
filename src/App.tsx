95ec6fed4c4f87fbe80c5cae47caa51b3fe82336
Nikolay Stoynov <arvigeus@gmail.com>
2019-05-07 00:20:53 +0300

    Make meta tags static

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import React, { useState } from "react";
 
-import SEO from "components/SEO";
 import Loading from "components/Loading";
 import Gallery from "components/Gallery";
 
 import GlobalStyle from "./GlobalStyle";
-import image from "components/Endgame/thanos.jpg";
 import getData from "./data";
 
 const App: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [data, setData] = useState<Array<Person>>([]);
-
-  const headings = (
-    <>
-      <SEO
-        title="The Snap - unleash your inner Thanos"
-        description="Snap all the people who disappeared during the decimation. There is only one possible future where the «Avengers» can win!"
-        image={image}
-      />
-      <GlobalStyle />
-    </>
-  );
 
   if (loading) {
     getData().then(result => {
@@ -30,7 +17,7 @@ const App: React.FC = () => {
     });
     return (
       <>
-        {headings}
+        <GlobalStyle />
         <Loading />
       </>
     );
@@ -38,7 +25,7 @@ const App: React.FC = () => {
 
   return (
     <>
-      {headings}
+      <GlobalStyle />
       <Gallery data={data} />
     </>
   );
