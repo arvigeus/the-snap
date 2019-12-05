fe9f4afba4b542e3d8fd8663f31546177d4de98a
Nikolay Stoynov <arvigeus@gmail.com>
2019-12-06 00:05:55 +0200

    Update create-react-app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("renders learn react link", () => {
+  const { getByAltText } = render(<App />);
+  const imgElement = getByAltText("Marvel's The Avengers");
+  expect(imgElement).toBeInTheDocument();
 });
